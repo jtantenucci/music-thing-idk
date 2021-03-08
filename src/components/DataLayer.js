5e4ce688b6caa9e13c22b76c59904f1b95ee869b
Alice Griffin <agriffin1@uwm.com>
2021-03-08 17:01:10 -0500

    commented out datalayer for now

--- src/components/DataLayer.js
+++ src/components/DataLayer.js
@@ -1,12 +1,11 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
-
 //creates a data layer
 export const DataLayerContext = createContext();
 
 //takes initial state and reducer
 //children is what is wrapped inside
-export  const DataLayer = ({initialState, reducer, children}) => (
+export const DataLayer = ({initialState, reducer, children}) => (
     <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </DataLayerContext.Provider>
--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-ui/core';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
-      <DataLayer initialState reducer>
         <App />
-      </DataLayer>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
